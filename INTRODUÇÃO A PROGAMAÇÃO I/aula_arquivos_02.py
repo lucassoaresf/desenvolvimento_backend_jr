6c3a2fe0a59e1ba7dce5d761e8d50db51837cdbc
Lucas Soares <lucassoaresf@outlook.com>
2022-10-22 10:38:14 -0300

    Update aula_arquivos_02.py

--- INTRODUÇÃO A PROGAMAÇÃO I/aula_arquivos_02.py
+++ INTRODUÇÃO A PROGAMAÇÃO I/aula_arquivos_02.py
@@ -3,8 +3,11 @@ file = "path/arquivo.txt"
 
 # Abrindo um arquivo como leitura
 with open(file, "r", encoding="utf-8") as file_object:
+    conteudo = file_object.readlines()
+
+    print(conteudo)
 
     # Criando uma "repetição" para ler por linhas
-    for linha in file_object:
+    for linha in conteudo:
 
         print(linha.rstrip())
