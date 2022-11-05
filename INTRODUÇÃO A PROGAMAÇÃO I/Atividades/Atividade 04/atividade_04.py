b5af910b4dca501609a5f3a75ce5ba8bcadd56ae
Lucas Soares <lucassoaresf@outlook.com>
2022-11-05 14:20:53 -0300

    update atividade_04.py

---
+++ INTRODUÇÃO A PROGAMAÇÃO I/Atividades/Atividade 04/atividade_04.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+def chamar_api(lat, lon, nome_arquivo):
+    API_KEY = '602c1e7ee001b05b492dad01c648da1f'
+    url = (f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}")
+
+    resposta = requests.request("GET", url)
+    objetos = json.loads(resposta.text)
+
+    arquivo = nome_arquivo + ".json"
+
+    with open(arquivo, 'w', encoding='utf-8') as f:
+        json.dump(objetos, f)
+
+dl = {
+    'Tokyo': {'lat': 35.6894, 'lon': 139.692},
+    'NY': {'lat': 40.6643, 'lon': 73.9385},
+    'Madagascar': {'lat': -18.9141, 'lon': 47.5306}
+}
+
+for index in dl.keys():
+    chamar_api(
+        lat=dl[index]['lat'],
+        lon=dl[index]['lon'],
+        nome_arquivo=index)
+
+    print(f"{index}| {dl[index]['lat']} | {dl[index]['lon']}")
+    
