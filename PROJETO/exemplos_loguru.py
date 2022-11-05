5f8b7a2ec90e69c579c4e7ea7401198276f97762
Lucas Soares <lucassoaresf@outlook.com>
2022-11-05 10:59:28 -0300

    Create exemplos_loguru.py

---
+++ PROJETO/exemplos_loguru.py
@@ -0,0 +1,7 @@
+from loguru import logger
+
+logger.info("Esta é uma info!!!")
+logger.success("Este é um sucesso!!!")
+logger.error("Este é um error!!!")
+logger.warning("Este é um warning!!!")
+logger.exception("Esta é uma Exception!!!")
