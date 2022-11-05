86cf3b915031ab3dd70177e1b9a8df0bbde048d1
Lucas Soares <lucassoaresf@outlook.com>
2022-11-05 11:32:39 -0300

    Update projeto_cadastro.py

--- PROJETO/projeto_cadastro.py
+++ PROJETO/projeto_cadastro.py
@@ -24,71 +24,79 @@ def lista_cadastro(database):
 database = {}
 
 while True:
-    option = int(input(
-        "1 - Cadastrar uma pessoa\n"
-        "2 - Listar os cadastros\n"
-        "3 - Deletar um cadastro\n"
-        "4 - Alterar um cadastro\n"
-        "5 - Salvar o database\n"
-        "0 - Sair do Programa\n"
-        "Digite sua opção: "
-    ))
+    try:
+        option = int(input(
+            "1 - Cadastrar uma pessoa\n"
+            "2 - Listar os cadastros\n"
+            "3 - Deletar um cadastro\n"
+            "4 - Alterar um cadastro\n"
+            "5 - Salvar o database\n"
+            "0 - Sair do Programa\n"
+            "Digite sua opção: "
+        ))
 
-    if option == 1:
-        codigo = int(input("Digite seu código de segurança: "))
-        nome = input("Digite seu nome: ")
-        email = input("Digite seu email: ")
-        data = input("Digite o dia/mês do seu nascimento: ")
+        if option == 1:
+            codigo = int(input("Digite seu código de segurança: "))
+            nome = input("Digite seu nome: ")
+            email = input("Digite seu email: ")
+            data = input("Digite o dia/mês do seu nascimento: ")
 
-        database[codigo] = {"nome": nome,
-                            "email": email,
-                            "data": data}
-        print("\n")
-        logger.success("Cadastro realizado com sucesso!!!")
-
-
-    elif option == 2:
-        print(" --- LISTAGEM DE CADASTROS --- ")
-        lista_cadastro(database)
-        print("\n")
-        logger.info("Listagem finalizada.")
+            database[codigo] = {"nome": nome,
+                                "email": email,
+                                "data": data}
+            print("\n")
+            logger.success("Cadastro realizado com sucesso!!!")
 
 
-    elif option == 3:
-        print(" --- Selecione o item a ser deletado --- ")
-        lista_cadastro(database)
-        codigo = int(input("Digite o código a ser deletado: "))
-        del database[codigo]
-        print("\n")
-        logger.success("Deletado com sucesso!!!")
+        elif option == 2:
+            print(" --- LISTAGEM DE CADASTROS --- ")
+            lista_cadastro(database)
+            print("\n")
+            logger.info("Listagem finalizada.")
 
 
-    elif option == 4:
-        print(" --- Selecione o item a ser alterado --- ")
-        lista_cadastro(database)
-        codigo = int(input("Digite o código a ser alterado --- "))
-        op = int(input(
-            "1 - Nome\n"
-            "2 - Email\n"
-            "3 - Data\n"
-            "O que você deseja alterar: "))
-        print("\n")
-        logger.success("Alterações realizadas com sucesso!!!")
+        elif option == 3:
+            print(" --- Selecione o item a ser deletado --- ")
+            lista_cadastro(database)
+            codigo = int(input("Digite o código a ser deletado: "))
+            del database[codigo]
+            print("\n")
+            logger.success("Deletado com sucesso!!!")
 
 
-        if op == 1:
-            nome = input("Digite o novo nome: ")
-            database[codigo]['nome'] = nome
-        if op == 2:
-            email = input("Digite o novo email: ")
-            database[codigo]['email'] = email
-        if op == 3:
-            data = input("Digite o novo data: ")
-            database[codigo]['data'] = data
+        elif option == 4:
+            print(" --- Selecione o item a ser alterado --- ")
+            lista_cadastro(database)
+            codigo = int(input("Digite o código a ser alterado --- "))
+            op = int(input(
+                "1 - Nome\n"
+                "2 - Email\n"
+                "3 - Data\n"
+                "O que você deseja alterar: "))
+            print("\n")
+            logger.success("Alterações realizadas com sucesso!!!")
 
-    elif option == 5:
-        salvar_database(database)
-        logger.success("Database salva com sucesso!!!")
 
-    elif option == 0:
-        break
+            if op == 1:
+                nome = input("Digite o novo nome: ")
+                database[codigo]['nome'] = nome
+            if op == 2:
+                email = input("Digite o novo email: ")
+                database[codigo]['email'] = email
+            if op == 3:
+                data = input("Digite o novo data: ")
+                database[codigo]['data'] = data
+
+        elif option == 5:
+            salvar_database(database)
+            logger.success("Database salva com sucesso!!!")
+
+        elif option == 0:
+            break
+
+    except ValueError as error:
+    logger.error(f"Valor inválido: {error}")
+
+    except Exception as error:
+        logger.error(f"Error: {error}")
+        continue
