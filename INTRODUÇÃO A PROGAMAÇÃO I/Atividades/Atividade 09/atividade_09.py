e6e26229be16b966957c7ea5e7aa8d1963d7addd
Lucas Soares <lucassoaresf@outlook.com>
2022-11-05 15:01:28 -0300

    Update atividade_09.py

--- INTRODUÇÃO A PROGAMAÇÃO I/Atividades/Atividade 09/atividade_09.py
+++ INTRODUÇÃO A PROGAMAÇÃO I/Atividades/Atividade 09/atividade_09.py
@@ -2,13 +2,21 @@ import json
 import requests
 from deep_translator import GoogleTranslator
 
-url = "https://api.adviceslip.com/advice"
+def chamar_api():
+    url = "https://api.adviceslip.com/advice"
 
-rq = requests.request("GET", url)
-message = json.loads(rq.text)
-text = message['slip']["advice"]
+    rq = requests.request("GET", url)
+    message = json.loads(rq.text)
+    text = message['slip']["advice"]
 
-translate = GoogleTranslator(source='english', target='portuguese').translate(text)
+    translate = GoogleTranslator(source='english', target='portuguese').translate(text)
 
-print(f"Texto original: {text}")
-print(f"Texto traduzido para português: {translate}")
+    print(f"Texto original: {text}")
+    print(f"Texto traduzido para português: {translate}")
+
+for i in range(30):
+    chamar_api()
+
+
+
+
