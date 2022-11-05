996100df655c42bda65b9680aa25215071b029ea
Lucas Soares <lucassoaresf@outlook.com>
2022-11-05 15:32:54 -0300

    update

--- INTRODUÇÃO A PROGAMAÇÃO I/Atividades/Atividade 09/atividade_09.py
+++ INTRODUÇÃO A PROGAMAÇÃO I/Atividades/Atividade 09/atividade_09.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from deep_translator import GoogleTranslator
+from loguru import logger
 
 
 def chamar_api():
@@ -15,6 +16,18 @@ def chamar_api():
     print(f"Texto original: {text}")
     print(f"Texto traduzido para português: {translate}")
 
+    return text, translate
+def salver_traducao(texto, texto_traduzido):
+
+    try:
+        with open("frases_traduzidas.txt", 'a', encoding='utf-8') as f:
+            t = texto_traduzido + "; " + texto + "\n"
+            f.write(t)
+
+    except Exception as error:
+        logger.exception(error)
 
 for i in range(30):
-    chamar_api()
+    x, y = chamar_api()
+
+    salver_traducao(x, y)
